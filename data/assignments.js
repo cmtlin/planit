4698a88e3240ba6d5fd00a4ad3c1cc4130e6dced
planit <planit@tanjeffreyz.github.io>
2022-11-03 02:00:38 +0000

    Updated assignments

---
+++ data/assignments.js
@@ -0,0 +1,482 @@
+assignments = {
+  "Chem 3B (Fall 2022)": [
+    {
+      "title": "PLWS 00",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-08-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2181442/submissions/130839622",
+      "submitted": true
+    },
+    {
+      "title": "PLWS 01",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-08-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2181490/submissions/130930035",
+      "submitted": true
+    },
+    {
+      "title": "Week 2 Disc (MONDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-08-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2183257/submissions/131159943",
+      "submitted": true
+    },
+    {
+      "title": "Week 2 Disc (WEDNESDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-08-31T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "Week 2 Disc (FRIDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-02T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "PLWS 02",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-08-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2183302/submissions/131353083",
+      "submitted": true
+    },
+    {
+      "title": "PLWS 03",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2183333/submissions/131689285",
+      "submitted": true
+    },
+    {
+      "title": "Week 3 Disc (MONDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2213970/submissions/132078994",
+      "submitted": true
+    },
+    {
+      "title": "Week 3 Disc (WEDNESDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "Week 3 Disc (FRIDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "PLWS 04",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2213987/submissions/132277390",
+      "submitted": true
+    },
+    {
+      "title": "PLWS 05",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2213994/submissions/132698375",
+      "submitted": true
+    },
+    {
+      "title": "PLWS 06",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2230105/submissions/133538029",
+      "submitted": true
+    },
+    {
+      "title": "PLWS 07",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-15T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2230386/submissions/134061837",
+      "submitted": true
+    },
+    {
+      "title": "Week 4 Disc (MONDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2247599/submissions/133215123",
+      "submitted": true
+    },
+    {
+      "title": "Week 4 Disc (WEDNESDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "Week 4 Disc (FRIDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-16T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "Week 5 Disc (MONDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-19T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2253879/submissions/134826326",
+      "submitted": true
+    },
+    {
+      "title": "Week 5 Disc (WEDNESDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "Week 5 Disc (FRIDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "PLWS 08",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2253896/submissions/135231537",
+      "submitted": true
+    },
+    {
+      "title": "PLWS 09",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-22T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2253922/submissions/135480543",
+      "submitted": true
+    },
+    {
+      "title": "Week 6 Disc (MONDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-26T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2269813/submissions/136402830",
+      "submitted": true
+    },
+    {
+      "title": "Week 6 Disc (WEDNESDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "Week 6 Disc (FRIDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "PLWS 10",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2269830/submissions/136832713",
+      "submitted": true
+    },
+    {
+      "title": "PLWS 11",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2269831/submissions/137739855",
+      "submitted": true
+    },
+    {
+      "title": "PLWS 09.5 - Exam 1 Study Guide",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-09-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2269986/submissions/137129619",
+      "submitted": true
+    },
+    {
+      "title": "Week 7 Disc (MONDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "Week 7 Disc (WEDNESDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-05T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "Week 7 Disc (FRIDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "PLWS 12",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2312920/submissions/138531832",
+      "submitted": true
+    },
+    {
+      "title": "PLWS 13",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2312921/submissions/139131580",
+      "submitted": true
+    },
+    {
+      "title": "Week 8 Disc (MONDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2321368/submissions/139778861",
+      "submitted": true
+    },
+    {
+      "title": "Week 8 Disc (WEDNESDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "Week 8 Disc (FRIDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "PLWS 14",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-11T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2321835/submissions/140147806",
+      "submitted": true
+    },
+    {
+      "title": "PLWS 15",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2321848/submissions/140943834",
+      "submitted": true
+    },
+    {
+      "title": "Exam 1 Analysis",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2327412/submissions/140449811",
+      "submitted": true
+    },
+    {
+      "title": "PLWS 16",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-18T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2339040/submissions/141968415",
+      "submitted": true
+    },
+    {
+      "title": "PLWS 17",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2339045/submissions/143043120",
+      "submitted": true
+    },
+    {
+      "title": "Week 9 Disc (MONDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-17T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2339046/submissions/141546676",
+      "submitted": true
+    },
+    {
+      "title": "Week 9 Disc (WEDNESDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-19T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "Week 9 Disc (FRIDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "Week 10 Disc (MONDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2361452/submissions/143448489",
+      "submitted": true
+    },
+    {
+      "title": "Week 10 Disc (WEDNESDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-26T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "Week 10 Disc (FRIDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "PLWS 18",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2361480/submissions/143907938",
+      "submitted": true
+    },
+    {
+      "title": "PLWS 19",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2361481/submissions/144702416",
+      "submitted": true
+    },
+    {
+      "title": "Week 11 Disc (MONDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-10-31T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2382455/submissions/145265995",
+      "submitted": true
+    },
+    {
+      "title": "Week 11 Disc (WEDNESDAY SECTIONS)",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-11-02T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "PLWS 20",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-11-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702/assignments/2382559/submissions/145773653",
+      "submitted": true
+    },
+    {
+      "title": "PLWS 21",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-11-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    },
+    {
+      "title": "PLWS 20.5 - Exam 2 Study Guide",
+      "course": "Chem 3B (Fall 2022)",
+      "dueDate": "2022-11-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/423702",
+      "submitted": false
+    }
+  ],
+  "MATH 1B": [
+    {
+      "title": "Homework 1",
+      "course": "MATH 1B",
+      "dueDate": "2022-08-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202136/submissions/131018136",
+      "submitted": true
+    },
+    {
+      "title": "Homework 2",
+      "course": "MATH 1B",
+      "dueDate": "2022-09-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202150/submissions/131880441",
+      "submitted": true
+    },
+    {
+      "title": "Homework 3",
+      "course": "MATH 1B",
+      "dueDate": "2022-09-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202151/submissions/133352487",
+      "submitted": true
+    },
+    {
+      "title": "Homework 4",
+      "course": "MATH 1B",
+      "dueDate": "2022-09-22T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202152/submissions/134621293",
+      "submitted": true
+    },
+    {
+      "title": "Homework 5",
+      "course": "MATH 1B",
+      "dueDate": "2022-09-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202153/submissions/136140157",
+      "submitted": true
+    },
+    {
+      "title": "Homework 6",
+      "course": "MATH 1B",
+      "dueDate": "2022-10-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202154/submissions/138151919",
+      "submitted": true
+    },
+    {
+      "title": "Homework 7",
+      "course": "MATH 1B",
+      "dueDate": "2022-10-11T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202155/submissions/139503949",
+      "submitted": true
+    },
+    {
+      "title": "Homework 8",
+      "course": "MATH 1B",
+      "dueDate": "2022-10-18T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202156/submissions/141996591",
+      "submitted": true
+    },
+    {
+      "title": "Homework 9",
+      "course": "MATH 1B",
+      "dueDate": "2022-10-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202157/submissions/143251751",
+      "submitted": true
+    },
+    {
+      "title": "Homework 10",
+      "course": "MATH 1B",
+      "dueDate": "2022-11-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202158/submissions/145147810",
+      "submitted": true
+    },
+    {
+      "title": "Project 1",
+      "course": "MATH 1B",
+      "dueDate": "2022-09-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202199/submissions/132408218",
+      "submitted": true
+    },
+    {
+      "title": "Project 2",
+      "course": "MATH 1B",
+      "dueDate": "2022-09-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202218/submissions/137697116",
+      "submitted": true
+    },
+    {
+      "title": "Project 3",
+      "course": "MATH 1B",
+      "dueDate": "2022-10-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777/assignments/2202219/submissions/141127780",
+      "submitted": true
+    },
+    {
+      "title": "Project 4",
+      "course": "MATH 1B",
+      "dueDate": "2022-11-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/422777",
+      "submitted": false
+    }
+  ]
+};
