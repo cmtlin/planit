6eb77e3fab86b9c10dadd1eb1149bb44c5c96144
planit <planit@tanjeffreyz.github.io>
2022-11-11 23:48:21 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -410,8 +410,8 @@ assignments = {
       "title": "PLWS 22",
       "course": "Chem 3B (Fall 2022)",
       "dueDate": "2022-11-11T23:59:00",
-      "link": "https://www.gradescope.com/courses/423702",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/423702/assignments/2403863/submissions/148235790",
+      "submitted": true
     },
     {
       "title": "PLWS 23",
