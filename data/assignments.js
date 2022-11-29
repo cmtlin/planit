9f11050b041db8bf4bb1647f5b238b4c430d835f
planit <planit@tanjeffreyz.github.io>
2022-11-29 16:56:05 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -465,7 +465,7 @@ assignments = {
     {
       "title": "Week 15 Disc (MONDAY SECTIONS)",
       "course": "Chem 3B (Fall 2022)",
-      "dueDate": "2022-11-28T23:59:00",
+      "dueDate": "2022-11-29T23:59:00",
       "link": "https://www.gradescope.com/courses/423702/assignments/2463056/submissions/151154388",
       "submitted": true
     },
