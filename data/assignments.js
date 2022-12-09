722635d96d2ee589411dbe19b963d4ddaac702b0
planit <planit@tanjeffreyz.github.io>
2022-12-09 23:36:44 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -501,8 +501,8 @@ assignments = {
       "title": "PLWS 27.5 - Unit 3 Study Guide",
       "course": "Chem 3B (Fall 2022)",
       "dueDate": "2022-12-11T23:59:00",
-      "link": "https://www.gradescope.com/courses/423702",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/423702/assignments/2463075/submissions/153607022",
+      "submitted": true
     }
   ],
   "MATH 1B": [
